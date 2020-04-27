a03ae34756104e6c8e398c774853fa8a605ffe60
Bartlomiej Puget <larhard@gmail.com>
2020-04-27 16:40:08 +0000

    Create service.sh

---
+++ service.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+script=$(readlink -f "$0")
+basedir=$(dirname "$script")
+
+action=$1
+shift
+
+PIDFILE=$basedir/server.pid
+LOGFILE=$basedir/server.log
+
+EXEC="$basedir/server.out"
+
+case "$action" in
+    start)
+        /sbin/start-stop-daemon --pidfile "$PIDFILE" --start --background --make-pidfile --oknodo --exec "$EXEC" -- "$@"
+        ;;
+    stop)
+        /sbin/start-stop-daemon --pidfile "$PIDFILE" --stop --signal TERM --oknodo --exec "$EXEC"
+        ;;
+    restart)
+        "$script" stop
+        "$script" start
+        ;;
+    nodaemon)
+        "$EXEC" "$@"
+        ;;
+    *)
+        echo "Unknown action: $action"
+        ;;
+esac
